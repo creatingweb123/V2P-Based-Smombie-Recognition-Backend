60daa6fe3e60e3f689c3e13e92aa7e48094663a6
sujnkim <blackout3210@naver.com>
2024-04-15 13:46:02 +0900

    Init: project setting

---
+++ src/main/java/com/capston/v2psmombie/V2pSmombieApplication.java
@@ -0,0 +1,13 @@
+package com.capston.v2psmombie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V2pSmombieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V2pSmombieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capston/v2psmombie/V2pSmombieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capston.v2psmombie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V2pSmombieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
