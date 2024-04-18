b7a43efeb1c3585df129e77abb0a7949fb787873
sujnkim <blackout3210@naver.com>
2024-04-18 18:13:19 +0900

    feat: AP 관련 컨트롤러 swagger 명세 작성

--- src/main/java/com/capston/v2psmombie/controller/ApController.java
+++ src/main/java/com/capston/v2psmombie/controller/ApController.java
@@ -3,6 +3,10 @@ package com.capston.v2psmombie.controller;
 import com.capston.v2psmombie.domain.Ap;
 import com.capston.v2psmombie.dto.ApCreateDto;
 import com.capston.v2psmombie.service.ApService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "Ap", description = "Ap 관련 REST API에 대한 명세")
 public class ApController {
 
     private final ApService apService;
@@ -21,6 +26,11 @@ public class ApController {
      * Ap 등록
      **/
     @PostMapping("/ap")
+    @Operation(summary = "Ap 생성(create)", description = "Ap를 생성합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "[SUCCESS] AP 생성"),
+            @ApiResponse(responseCode = "400", description = "[FAIL] AP 생성")
+    })
     public ResponseEntity<String> createAp(@RequestBody ApCreateDto dto) {
         try {
             Ap createdAp = apService.create(dto);
