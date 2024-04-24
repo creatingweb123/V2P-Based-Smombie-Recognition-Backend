2088e369b792dddf5e52ca780b2d78d61c1464cc
creatingweb123 <71700107+creatingweb123@users.noreply.github.com>
2024-04-24 18:55:58 +0900

    fit: 정지거리 비교 함수 삭제
    정지시간과 만나는 시간을 비교하지 말고
    두 객체가 만나는 시간을 통해 risk 판단하는게 낫다고 판단

--- src/main/java/com/capston/v2psmombie/riskCalculate/RiskCalculator.java
+++ src/main/java/com/capston/v2psmombie/riskCalculate/RiskCalculator.java
@@ -12,18 +12,13 @@ public class RiskCalculator {
     private List<User> smombieDataList;
     private User carData;
 
-    @Builder.Default
-    private double carDeceleration = 1.0;
-
     @Builder
     public RiskCalculator(
             List<User> smombieDataList,
-            User carData,
-            double carDeceleration
+            User carData
     ) {
         this.smombieDataList = smombieDataList;
         this.carData = carData;
-        this.carDeceleration = carDeceleration;
     }
 
     public static RiskCalculatorBuilder builder(List<User> users, User car){
@@ -35,11 +30,7 @@ public class RiskCalculator {
 
 
     public int riskCheck() {
-        double dif = findMinTime() - calculateTimeToStop(
-                calculateTotalStoppingDistance(carData.getSpeed(), 1),
-                carDeceleration
-        );
-
+        double dif = findMinTime();
         if (dif > 30) {
             return 3;
         } else if (dif > 10) {
@@ -48,29 +39,6 @@ public class RiskCalculator {
             return 1;
         }
     }
-
-    /* speed: km/h, reactionTime: s */
-    private double calculateBrakeReactionDistance(double speed, double reactionTime) {
-        return (speed * reactionTime * 3.6) / 1000;
-        // Convert km/h to m/s
-    }
-
-    // Function to calculate Braking Distance
-    private double calculateBrakingDistance(double speed) {
-        return (speed * speed * 0.4) / 1000;
-        // Convert km/h to m/s
-    }
-
-    private double calculateTotalStoppingDistance(double speed, double reactionTime) {
-        double brakeReactionDistance = calculateBrakeReactionDistance(speed, reactionTime);
-        double brakingDistance = calculateBrakingDistance(speed);
-        return brakeReactionDistance + brakingDistance;
-    }
-
-    private double calculateTimeToStop(double totalStoppingDistance, double deceleration) {
-        return Math.sqrt((-2 * totalStoppingDistance) / deceleration);
-    }
-
     private double findMinTime() {
         double minMeetingTime = Double.MAX_VALUE;
         for (User user : smombieDataList) {
